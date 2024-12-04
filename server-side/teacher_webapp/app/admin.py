77aa837541968154f22e4f8d3840c3ca82e77d62
Alexanderms36 <alexanderms3646@gmail.com>
2024-12-04 17:02:20 +0500

    fixes in admin panel

--- server-side/teacher_webapp/app/admin.py
+++ server-side/teacher_webapp/app/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Student, User, Classes
 
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if obj.password:
+            obj.set_password(obj.password)
+        obj.save()
+
+admin.site.register(User, UserAdmin)
 admin.site.register(Student)
 admin.site.register(Classes)
